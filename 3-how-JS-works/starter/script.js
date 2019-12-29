b87a54759db0002b0db211a403461f8b3b48b3f7
cln-dot <cln-dot@users.noreply.github.com>
2019-12-29 11:33:49 -0800

    Hoisting, scoping, and this lessons

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,21 +1,36 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+calculateAge(1989);
 
 
+function calculateAge(birthYear) {
+    console.log(2019 - birthYear);
+}
 
+calculateAge(1990);
 
+// Doesn't work because below is a function expression
+// yearsUntilRetirement(1990);
 
+var yearsUntilRetirement = function(birthYear) {
+    console.log(65 - (2019 - birthYear));
+}
 
+yearsUntilRetirement(1990);
 
+// variables
+console.log(age);
+var age = 28;
+console.log(age);
 
+function foo(){
+    var age = 33;
+    console.log(age);
+}
 
-
-
-
-
-
-
+foo();
+console.log(age);
 
 ///////////////////////////////////////
 // Lecture: Scoping
@@ -23,7 +38,7 @@
 
 // First scoping example
 
-/*
+
 var a = 'Hello!';
 first();
 
@@ -36,8 +51,11 @@ function first() {
         console.log(a + b + c);
     }
 }
-*/
 
+function fourth() {
+    var d = 'John';
+    console.log(a + d);
+}
 
 
 // Example to show the differece between execution stack and scope chain
@@ -67,11 +85,38 @@ function third() {
 ///////////////////////////////////////
 // Lecture: The this keyword
 
+console.log(this);
 
+// calculateAge(1989);
 
+// function calculateAge(birthYear) {
+//     console.log(2019 - birthYear);
+//     console.log(this);
+// }
 
+var john = {
+    firstName: 'John',
+    lastName: 'Smith',
+    birthYear: 1989,
+    calculateAge: function(){
+        console.log(this);
+        console.log(2019 - this.birthYear);
 
+        // function innerFunction(){
+        //     console.log(this);
+        // }
+        // innerFunction();
+    }
+}
 
+john.calculateAge();
 
+var mark = {
+    firstName: 'Mark',
+    birthYear: 1984
+}
+
+mark.calculateAge = john.calculateAge;
+mark.calculateAge();
 
 
